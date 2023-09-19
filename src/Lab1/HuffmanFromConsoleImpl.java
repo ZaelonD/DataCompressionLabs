8b20c5ea2d8c128aa0de1388ee6b896005bf15a0
Александр Дубровин <xah799@mail.ru>
2023-09-19 16:26:36 +0300

    Created HuffmanFromConsoleImpl class

--- src/Lab1/HuffmanFromConsoleImpl.java
+++ src/Lab1/HuffmanFromConsoleImpl.java
@@ -1,9 +1,13 @@
 package Lab1;
 
+import java.util.TreeMap;
+
 public class HuffmanFromConsoleImpl implements HuffmanLogic {
 
     @Override
     public void runHuffman() {
-
+        ReadingTextFromConsole read = new ReadingTextFromConsole();
+        Frequency frequencies = new Frequency();
+        TreeMap<Character, Integer> freq = frequencies.countFrequency(read.getText());
     }
 }
