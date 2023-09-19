5db87771c4476710ad325df1e8c1e89953476aaf
Александр Дубровин <xah799@mail.ru>
2023-09-19 11:48:11 +0300

    Overridden readTheText() method, added getText() method

--- src/Lab1/ReadingTextFromConsole.java
+++ src/Lab1/ReadingTextFromConsole.java
@@ -1,4 +1,24 @@
 package Lab1;
 
-public class ReadingTextFromConsole {
+import java.util.Scanner;
+
+public class ReadingTextFromConsole implements ReadService {
+
+    private final Scanner SCANNER;
+    private String text;
+
+    public ReadingTextFromConsole() {
+        System.out.println("Введите текст, который хотите закодировать");
+        SCANNER = new Scanner(System.in);
+    }
+
+    @Override
+    public void readTheText() {
+        text = SCANNER.nextLine();
+        SCANNER.close();
+    }
+
+    public String getText() {
+        return text;
+    }
 }
